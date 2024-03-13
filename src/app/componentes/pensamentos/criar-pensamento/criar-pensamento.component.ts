5255199a8f530a8abd431a9614d47955bacd2a13
André Guimarães <andrepaivaguimaraes@gmail.com>
2024-03-13 08:50:22 -0300

    banners e itens cabecalho

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrl: './criar-pensamento.component.css'
+})
+export class CriarPensamentoComponent {
+
+}
